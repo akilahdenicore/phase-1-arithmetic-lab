6338917aa83152738791f5447ca7b7c3747ecf77
Akilah Snowden <akilahdsnowden@gmail.com>
2022-04-25 22:16:01 -0400

    commit tree ca23dffc952f7ff27f9b1dd999a74016085a8baf

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
-// Write your code here
+/*multiply
+is an equation whose multiplied results will equal 62:
+ReferenceError: num1 is not defined*/
+
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+console.log(multiply);
+/* random
+generates a random integer greater than 0:
+ReferenceError: random is not defined */
+
+const random = Math.ceil(Math.random());
+
+/*   
+mod is an equation that calculates a remainder that is equal to 4:
+ ReferenceError: num3 is not defined
+*/
+
+let num3 = 19;
+let num4 = 5;
+const mod = num3 % num4; 
+
+/* max
+         will return 20 as the highest number in the set:
+     ReferenceError: max is not defined
+     */
+
+const max = Math.max(3, 7, 11, 20);
